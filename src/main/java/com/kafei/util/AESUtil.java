0ad215b7a19e5589d235a57a22f48dc606e11957
tianKuiJ <m18608086909@163.com>
2019-04-15 16:14:02 +0800

    update AESUtil.java 2

--- src/main/java/com/kafei/util/AESUtil.java
+++ src/main/java/com/kafei/util/AESUtil.java
@@ -65,7 +65,7 @@ public class AESUtil {
 	}
 
 	public static void main(String[] args) throws Exception {
-//		System.out.println(AESUtil.getInstance().decrypt("YYAwP+m3mlm/wIYQ3slpfw=="));
+		System.out.println(AESUtil.getInstance().decrypt("YYAwP+m3mlm/wIYQ3slpfw=="));
 //		System.out.println(AESUtil.getInstance().encrypt("123455"));
 //		String a = "http://localhost:8080/files/EC3DFDCB4E361.jpg";
 //		System.out.println(a.lastIndexOf("/")+1);//获取到文件名称);
