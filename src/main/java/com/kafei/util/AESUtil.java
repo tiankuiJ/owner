5d8f89c8809d222a9dbcadc1a945daaef17d9ba8
tianKuiJ <m18608086909@163.com>
2019-04-17 13:13:07 +0800

    df

--- src/main/java/com/kafei/util/AESUtil.java
+++ src/main/java/com/kafei/util/AESUtil.java
@@ -51,7 +51,7 @@ public class AESUtil {
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 			IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
 			cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-			byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);// 先用base64解密
+			byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);
 			byte[] original = cipher.doFinal(encrypted1);
 			String originalString = new String(original, "utf-8");
 			return originalString;
